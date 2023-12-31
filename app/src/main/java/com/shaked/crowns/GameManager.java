6f6a125f86423c77be84285175792a276979d3c6
lonaim <shakednaim2006@gmail.com>
2023-12-31 14:35:39 +0200

    creat gameManager with turns

--- app/src/main/java/com/shaked/crowns/GameManager.java
+++ app/src/main/java/com/shaked/crowns/GameManager.java
@@ -2,6 +2,8 @@ package com.shaked.crowns;
 
 import android.app.Activity;
 
+import java.util.ArrayList;
+
 public class GameManager {
 
 
@@ -9,7 +11,7 @@ public class GameManager {
     int kTurn; //index, Decides which player to turn
     private Deck gameDeck;//the main deck of the game
     private Deck burnDeck;// the burn deck
-    private Card deckCard;// a card from the deck
+    private Card deckCard;// a card from the deck that the player will use
     private Activity activity;//the activity
 
     /*פעולה בונה*/
@@ -20,8 +22,8 @@ public class GameManager {
 
         this.deckCard = new Card(0, "");
         this.players = new Player[2];
-        players[0] = new Player("Player 1", new Siege(new Card(12,"c"), new Card(11,"c"), gameDeck));
-        players[1] = new Player("Player 2", new Siege(new Card(12,"h"), new Card(11,"h"), gameDeck));
+        players[0] = new Player("Player 1", new Siege(new Card(13,"c"), new Card(12,"c"), gameDeck));
+        players[1] = new Player("Player 2", new Siege(new Card(13,"h"), new Card(12,"h"), gameDeck));
         this.kTurn = 0;
     }
 
@@ -71,7 +73,6 @@ public class GameManager {
         return players[0];
     }
 
-
     public Player getPlayer(int kTurn) {
         return players[kTurn];
     }
@@ -81,23 +82,111 @@ public class GameManager {
         return this.gameDeck;
     }
 
+    //update the name of the players
     public void setPlayersName(String p1, String p2) {
         players[0].setName(p1);
         players[1].setName(p2);
     }
 
+    //move the turn to the next player
     public void nextTurn() {
         if(this.kTurn == 0) this.kTurn = 1;
         else this.kTurn = 0;
     }
 
-    public void doBurn() {
-        burnDeck.addCard(deckCard);
+    //get a card and put it in the burn deck
+    public void doBurn(Card burnCard) {
+        burnDeck.addCard(burnCard);
     }
 
+    //use - reset the deck after all burn
+    //move all the cards from the burn deck to the game deck
     public void resetDeckAfterAllBurn() {
         gameDeck = burnDeck.cloneDeck();
         burnDeck = new Deck();
         gameDeck.shuffle();
     }
+
+    /*actions player can do*/
+
+    //atteck action
+    public Boolean doAtteck(Tower towerDiffend){
+        Card towerCard = towerDiffend.getCard();
+        
+        if((deckCard.equalsNum(1) && towerCard.equalsNum(13))
+                ||(deckCard.equalsNum(towerCard.getNum()+1))){
+                towerDiffend.removeCard();
+                doBurn(deckCard);
+                doBurn(towerCard);
+                if(towerDiffend.getSize() == 0){
+                    towerDiffend=new Tower(new Card(0,""));
+                }
+            return true;
+        }
+        return false;
+    }
+
+    //fortify action
+    public Boolean dofortify(Tower tower) {
+        Card towerCard = tower.getCard();
+        if (!tower.isFull()) {
+            if (tower.getCard().equalsNum(deckCard.getNum())) {
+                tower.addCard(deckCard);
+                deckCard = null;
+                return true;
+            } else if (tower.getCard().equalsNum(0) && deckCard.equalsNum(1)) {
+                tower.getCard().setNum(1);
+                tower.getCard().setShape(deckCard.getShape());
+                deckCard = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /*turn action*/
+
+    //return if the turn was done
+    public boolean makeATurn(int i/*place of the card in the tower*/, int action/*which action is*/) {
+        ArrayList<Tower> line;
+        ArrayList<Tower> enemyLine;
+
+        if((!deckCard.equalsNum(0) || deckCard != null) && (gameDeck.getSize() != 0)) {
+            if(i < 3) {/*line 1*/
+                line = getPlayerTurn().getSiege().getLine1();
+                enemyLine = getPlayerNotTurn().getSiege().getLine1();
+                switch (action) {
+                    case 2://fortify
+                        dofortify(line.get(i));
+                    case 3://atteck
+                        doAtteck(enemyLine.get(i));
+                }//end of switch
+            }/*end of line 1*/
+
+            if(i >= 3 && i < 7) {/*line 2*/
+                line = getPlayerTurn().getSiege().getLine2();
+                enemyLine = getPlayerNotTurn().getSiege().getLine2();
+                switch (action) {
+                    case 2://fortify
+                        dofortify(line.get(i-3));
+                    case 3://atteck
+                        doAtteck(enemyLine.get(i-3));
+                }//end of switch
+            }/*end of line 2*/
+
+            if(i >= 7 && i < 9) {/*line 3*/
+                line = getPlayerTurn().getSiege().getQk();
+                enemyLine = getPlayerNotTurn().getSiege().getQk();
+                switch (action) {
+                    case 2://fortify
+                        return false;
+                    case 3://atteck
+                        doAtteck(enemyLine.get(i-7));
+                }//end of switch
+            }/*end of line 3*/
+        }
+        return false;
+    }
+
 }
